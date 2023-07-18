91c4baf5e5fa041d57fdca45a49c63ad4c456236
Ibnballo1 <belloabdullateef035@gmail.com>
2023-07-18 04:00:42 +0100

    Solve bubble sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,16 @@
 def bubble_sort(arr)
-  (arr.length).times do |index|
-    (arr.length).times do |i|
-    #   puts "#{arr[i]}"
-      if (arr[i] > arr[i + 1]) && (i <= arr.length)
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
-      end
-      puts "arr is now: #{arr}"
+  arr_len = arr.length
+  breaker = arr_len - 1
+
+  (arr_len).times do
+    (arr_len).times do |i|
+      break if ((i == breaker) || (i == (arr_len - 1)))
+      (arr[i], arr[i + 1] = arr[i + 1], arr[i]) if (arr[i] > arr[i + 1])
     end
+    breaker -= 1
   end
-  # arr
+  arr
 end
 
 p bubble_sort([4,3,78,2,0,2]) # => [0,2,2,3,4,78]
+p bubble_sort([23,9,12,16,8]) # => [8,9,12,16,23]
